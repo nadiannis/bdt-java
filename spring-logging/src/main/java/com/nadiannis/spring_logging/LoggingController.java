6b6148b6276ef4c0e8927515eda1f5ce7a381f4a
nadiannis <annisanadianeyla@gmail.com>
2024-07-04 22:19:00 +0700

    Add spring-logging project

---
+++ spring-logging/src/main/java/com/nadiannis/spring_logging/LoggingController.java
@@ -0,0 +1,26 @@
+package com.nadiannis.spring_logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logger")
+public class LoggingController {
+
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @GetMapping
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Hello! Check out the logs to see the output...";
+    }
+
+}
